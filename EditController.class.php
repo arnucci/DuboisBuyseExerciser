ee051e86b0fff0c9bee1c11d9fcc418f76300a6a
arnucci <arnaud.salvucci@gmail.com>
2013-10-04 15:51:43 +0200

    refactoring data processing

--- EditController.class.php
+++ EditController.class.php
@@ -2,8 +2,94 @@
 
 class EditController
 {
-    public function viewsManagement()
+    private function processData()
     {
+        $arrayData = array();
+
+        $countDebut  = 0;
+        $countMilieu = 0;
+        $countFin    = 0;
+
+        if (!empty($_POST['debut'])) {
+
+            $debut = array();
+
+            $debut[] = 'Mots commençant par "'.$_POST['lettre'].'"';
+
+            foreach ($_POST['debut'] as $word) {
+
+                $debut[] = $word;
+            }
+                    
+            $arrayData['debut'] = $debut;
+            $countDebut = count($debut);
+        }
+
+        if (!empty($_POST['milieu'])) {
+
+            $milieu = array();
+			
+            $milieu[] = 'Mots contenant "'.$_POST['lettre'].'"';
+
+            foreach ($_POST['milieu'] as $word) {
+
+                $milieu[] = $word;
+            }
+
+            $arrayData['milieu'] = $milieu;
+            $countMilieu = count($milieu);
+        }
+
+        if (!empty($_POST['fin'])) {
+
+            $fin = array();
+
+            $fin[] = 'Mots finissant par "'.$_POST['lettre'].'"';
+            
+            foreach ($_POST['fin'] as $word) {
+
+                $fin[] = $word;
+            }
+            $arrayData['fin'] = $fin;
+            $countFin = count($fin);
+        }
+
+        $max = max($countDebut, $countMilieu, $countFin);
+
+        $i = 0;
+
+        $arrayRows = array();
+
+        while ($i <$max) {
+
+            if ($countDebut !== 0) {
+
+                if (array_key_exists('debut', $arrayData)) {
+
+                    $arrayRows[$i][] = (array_key_exists($i, $arrayData['debut'])) ? $arrayData['debut'][$i] : "";
+                }
+            }
+
+            if ($countMilieu !== 0) {
+
+                if (array_key_exists('milieu', $arrayData)) {
+
+                    $arrayRows[$i][] = (array_key_exists($i, $arrayData['milieu'])) ? $arrayData['milieu'][$i] : "";
+                }
+            }
+
+            if ($countFin !== 0) {
+
+                if (array_key_exists('fin', $arrayData)) {
+
+                    $arrayRows[$i][] = (array_key_exists($i, $arrayData['fin'])) ? $arrayData['fin'][$i] : "";
+                }
+            }
+
+            $i++;
+        }
+
+        return $arrayRows;
     }
 
     private function renderPdf()
@@ -74,92 +160,10 @@ class EditController
 
 
     private function renderExcel() {
-            
-        $csv = array();
 
-        $countDebut  = 0;
-        $countMilieu = 0;
-        $countFin    = 0;
-
-        if (!empty($_POST['debut'])) {
-
-            $debut = array();
-
-            $debut[] = 'Mots commençant par "'.$_POST['lettre'].'"';
-
-            foreach ($_POST['debut'] as $word) {
-
-                $debut[] = $word;
-            }
-                    
-            $csv['debut'] = $debut;
-            $countDebut = count($debut);
-        }
-
-        if (!empty($_POST['milieu'])) {
-
-            $milieu = array();
-			
-            $milieu[] = 'Mots contenant "'.$_POST['lettre'].'"';
-
-            foreach ($_POST['milieu'] as $word) {
-
-                $milieu[] = $word;
-            }
-            $csv['milieu'] = $milieu;
-            $countMilieu = count($milieu);
-        }
-
-        if (!empty($_POST['fin'])) {
-
-            $fin = array();
-
-            $fin[] = 'Mots finissant par "'.$_POST['lettre'].'"';
-            
-            foreach ($_POST['fin'] as $word) {
-
-                $fin[] = $word;
-            }
-            $csv['fin'] = $fin;
-            $countFin = count($fin);
-        }
-
-        $max = max($countDebut, $countMilieu, $countFin);
+        $arrayRows = $this-> processData();
 
         $fp = fopen('tmp/file.csv', 'w');
-        $i = 0;
-
-        $arrayRows = array();
-
-        while ($i <$max) {
-
-            if ($countDebut !== 0) {
-
-                if (array_key_exists('debut', $csv)) {
-
-                    $arrayRows[$i][] = (array_key_exists($i, $csv['debut'])) ? $csv['debut'][$i] : "";
-                }
-            }
-
-            if ($countMilieu !== 0) {
-
-                if (array_key_exists('milieu', $csv)) {
-
-                    $arrayRows[$i][] = (array_key_exists($i, $csv['milieu'])) ? $csv['milieu'][$i] : "";
-                }
-            }
-
-            if ($countFin !== 0) {
-
-                if (array_key_exists('fin', $csv)) {
-
-                    $arrayRows[$i][] = (array_key_exists($i, $csv['fin'])) ? $csv['fin'][$i] : "";
-                }
-            }
-
-            $i++;
-        }
-
         foreach ($arrayRows as $fields) {
 
             fputcsv($fp, $fields);
