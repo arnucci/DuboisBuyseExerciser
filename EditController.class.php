f6da6439fe651863030de586ac57429c25692ae7
arnucci <arnaud.salvucci@gmail.com>
2013-10-20 20:59:29 +0200

    remove header and footer in PDF file

--- EditController.class.php
+++ EditController.class.php
@@ -100,27 +100,11 @@ class EditController
         // create new PDF document
         $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-        // set document information
-        $pdf->SetCreator(PDF_CREATOR);
-        $pdf->SetAuthor('Nicola Asuni');
-        $pdf->SetTitle('TCPDF Example 007');
-        $pdf->SetSubject('TCPDF Tutorial');
-        $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-
-        // set default header data
-//        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 007', PDF_HEADER_STRING);
-
-        // set header and footer fonts
-        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
         // set default monospaced font
         $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
         // set margins
         $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
         // set auto page breaks
         $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
@@ -161,19 +145,19 @@ class EditController
         $y = $pdf->getY();
 
         // set color for background
-        $pdf->SetFillColor(255, 255, 200);
+        $pdf->SetFillColor(255, 255, 255);
 
         // set color for text
-        $pdf->SetTextColor(0, 63, 127);
+        $pdf->SetTextColor(0, 0, 0);
 
         // write the first column
         $pdf->writeHTMLCell(80, '', '', $y, $leftColumn, 1, 0, 1, true, 'J', true);
 
         // set color for background
-        $pdf->SetFillColor(215, 235, 255);
+        $pdf->SetFillColor(255, 255, 255);
 
         // set color for text
-        $pdf->SetTextColor(127, 31, 0);
+        $pdf->SetTextColor(0, 0, 0);
 
         // write the second column
         $pdf->writeHTMLCell(80, '', '', '', $rightColumn, 1, 1, 1, true, 'J', true);
