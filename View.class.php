1156ad6c24666b9cce637578fd77e86f4b88eca8
arnucci <arnaud.salvucci@gmail.com>
2014-08-23 15:18:52 +0200

    ajout d'une classe mère view

---
+++ View.class.php
@@ -0,0 +1,63 @@
+<?php
+
+class View
+{
+    private $_script;
+
+    private $_content;
+
+
+    /**
+     * Constructeur de la classe View
+     */
+    function __construct()
+    {
+    }
+
+
+    /**
+     * Initialise le(s) chemin des scripts et leur type
+     *
+     * @param array $script le tableau associatif nom-fichier => type
+     *
+     * @return empty
+     */
+    function initScript($script = null)
+    {
+        if ($script === null || !is_array($script)) {
+            $this->_script = '';
+        } else {
+            foreach ($script as $file => $type) {
+                $this->_script .= '<script type="'.$type.'" src="script/'.$file.'" ></script>'."\n\t\t";
+            }
+        }
+    }
+
+    /**
+     * Initialise le contenu de la vue
+     *
+     * @param string $content le contenu de la vue
+     *
+     * @return empty
+     */
+    function setContent($content = null)
+    {
+        $this->_content = $content;
+    }
+
+
+    /**
+     * Inclue le header à la vue
+     *
+     * @param string $content le contenu de la vue
+     * @param array  $script  les scripts associés à la vue
+     *
+     * @return empty
+     */
+    function setLayout($content = null, $script = null)
+    {
+        $this->setContent($content);
+        $this->initScript($script);
+        require_once 'layout.php';
+	}
+}
